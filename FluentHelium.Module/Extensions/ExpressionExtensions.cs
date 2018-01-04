6c44b13df1b31dcb048b0107a45ad083595dec89
Kirill Maurin <leo.bonart@gmail.com>
2018-01-04 07:12:33 +0300

    Add code coverage CI test

---
+++ FluentHelium.Module/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace FluentHelium.Module
+{
+    public static class ExpressionExtensions
+    {
+        public static Expression Convert(this Expression expression, Type type)
+        {
+            return Expression.Convert(expression, type);
+        }
+
+        public static Expression Convert<T>(this Expression expression)
+        {
+            return Expression.Convert(expression, typeof(T));
+        }
+
+        public static Expression Get(this Expression expression, PropertyInfo property)
+        {
+            return Expression.Property(expression, property);
+        }
+    }
+}
