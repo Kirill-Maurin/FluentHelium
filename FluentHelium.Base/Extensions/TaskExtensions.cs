ac098e061dca2d2d0a4e5657cda193122d4331ab
Kirill Maurin <leo.bonart@gmail.com>
2018-12-04 03:57:04 +0300

    Add LINQ for tasks

--- FluentHelium.Base/Extensions/TaskExtensions.cs
+++ FluentHelium.Base/Extensions/TaskExtensions.cs
@@ -37,5 +37,116 @@ namespace FluentHelium.Base
             exception = default;
             return task.IsFaulted && task.AsTask().TryGetException(out exception);
         }
+
+        public static Task<TResult> SelectMany<T, TResult>(this Task<T> task, Func<T, Task<TResult>> selector)
+        {
+            if (task.CheckCanceled<TResult>(out var r) || task.CheckException(out r))
+                return r;
+            var tcs = new TaskCompletionSource<TResult>();
+            task.SelectManyWithSource(selector, tcs);
+            return tcs.Task;
+        }
+
+        public static Task<TResult> Select<T, TResult>(this Task<T> task, Func<T, TResult> selector)
+        {
+            if (task.CheckCanceled<TResult>(out var r) || task.CheckException(out r))
+                return r;
+            var tcs = new TaskCompletionSource<TResult>();
+            task.SelectWithSource(selector, tcs);
+            return tcs.Task;
+        }
+
+        public static Task<T> Do<T>(this Task<T> task, Action<T> action)
+            => task.Select(t =>
+            {
+                action(t);
+                return t;
+            });
+
+        public static Task<TResult> Using<T, TResult>(this Task<T> task, Func<T, Task<TResult>> selector)
+            where T : IDisposable
+            => task.SelectMany(r => selector(r).Do(t => r.Dispose()));
+
+        static void SelectWithSource<T, TResult>(
+            this Task<T> task,
+            Func<T, TResult> selector,
+            TaskCompletionSource<TResult> tcs)
+        {
+            if (!task.IsCompleted)
+                task.ContinueWith(t =>
+                {
+                    if (!task.CheckCanceled(tcs) && !task.CheckException(tcs))
+                        task.SelectWithSource(selector, tcs);
+                });
+
+            try
+            {
+                tcs.TrySetResult(selector(task.Result));
+            }
+            catch (Exception e)
+            {
+                tcs.TrySetException(e);
+            }
+        }
+
+        static void SelectManyWithSource<T, TResult>(
+            this Task<T> task,
+            Func<T, Task<TResult>> selector,
+            TaskCompletionSource<TResult> tcs)
+        {
+            if (!task.IsCompleted)
+                task.ContinueWith(t =>
+                {
+                    if (!task.CheckCanceled(tcs) && !task.CheckException(tcs))
+                        task.SelectManyWithSource(selector, tcs);
+                });
+            try
+            {
+                selector(task.Result).ToCompletionSource(tcs);
+            }
+            catch (Exception e)
+            {
+                tcs.TrySetException(e);
+            }
+        }
+
+        public static bool CheckCanceled<T>(this Task task, out Task<T> result)
+        {
+            var c = task.IsCanceled;
+            result = c ? Canceled<T>() : default;
+            return c;
+        }
+
+        public static bool CheckException<T>(this Task task, out Task<T> result)
+        {
+            var c = task.TryGetException(out var e);
+            result = c ? Task.FromException<T>(e) : default;
+            return c;
+        }
+
+        public static bool CheckCanceled<T>(this Task task, TaskCompletionSource<T> tcs)
+        {
+            var result = task.IsCanceled;
+            if (result)
+                tcs.TrySetCanceled();
+            return result;
+        }
+
+        public static bool CheckException<T>(this Task task, TaskCompletionSource<T> tcs)
+        {
+            var result = task.TryGetException(out var e);
+            if (result)
+                tcs.TrySetException(e);
+            return result;
+        }
+
+        public static Task<T> ToCompletionSource<T>(this Task<T> task, TaskCompletionSource<T> tcs)
+        {
+            if (!task.CheckCanceled(tcs) && !task.CheckException(tcs) && task.IsCompleted)
+                tcs.TrySetResult(task.Result);
+            else
+                task.ContinueWith(t => t.ToCompletionSource(tcs));
+            return tcs.Task;
+        }
     }
 }
--- FluentHelium.Benchmarks/Program.cs
+++ FluentHelium.Benchmarks/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Threading.Tasks;
-using BenchmarkDotNet.Attributes;
+﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Toolchains.InProcess;
 using FluentHelium.Base;
+using System;
+using System.Threading.Tasks;
 using TaskExtensions = FluentHelium.Base.TaskExtensions;
 
 namespace FluentHelium.Benchmarks
@@ -21,15 +21,20 @@ namespace FluentHelium.Benchmarks
     public sealed class Benchmarks
     {
         [Benchmark]
-        public Exception AwaitCanceledBencmark() => TestTask(TaskExtensions.Canceled<Unit>()).Exception;
+        public Exception AwaitCanceledBenchmark() => TestTask(TaskExtensions.Canceled<Unit>()).Exception;
 
         [Benchmark]
-        public Exception AwaitCanceledResultBencmark() => TestWithResult(TaskExtensions.Canceled<Unit>()).Exception;
+        public Exception AwaitCanceledResultBenchmark() => TestWithResult(TaskExtensions.Canceled<Unit>()).Exception;
+
+        [Benchmark]
+        public Exception SelectCanceledTaskBenchmark() => TestWithTask(TaskExtensions.Canceled<Unit>()).Exception;
 
         async Task<Unit> TestTask(Task<Unit> inner) => await inner.ConfigureAwait(false);
 
         async Task<Result<Unit>> TestResult(Task<Unit> inner) => await inner.ToResult();
         Task<Unit> TestWithResult(Task<Unit> inner) => TestResult(inner).ToResult().ToTask();
+
+        Task<Unit> TestWithTask(Task<Unit> inner) => inner.Select(t => t);
     }
 
     public static class Program
